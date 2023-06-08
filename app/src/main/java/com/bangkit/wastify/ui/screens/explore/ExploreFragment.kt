72b7a6cd9a91d7a765dbcde602cb89a4fb6b0b95
Isfan <isfan.adheltyo@gmail.com>
2023-06-08 13:00:55 +0700

    feat(explore): add google map

--- app/src/main/java/com/bangkit/wastify/ui/screens/explore/ExploreFragment.kt
+++ app/src/main/java/com/bangkit/wastify/ui/screens/explore/ExploreFragment.kt
@@ -1,20 +1,91 @@
 package com.bangkit.wastify.ui.screens.explore
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.bangkit.wastify.R
+import com.bangkit.wastify.databinding.FragmentExploreBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import dagger.hilt.android.AndroidEntryPoint
 
-class ExploreFragment : Fragment() {
+@AndroidEntryPoint
+class ExploreFragment : Fragment(), OnMapReadyCallback, GoogleMap.InfoWindowAdapter {
+
+    private var _binding: FragmentExploreBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var map: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+    ): View {
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = (childFragmentManager.findFragmentById(R.id.map_view) as SupportMapFragment)
+        mapFragment.getMapAsync(this)
+
+        getMyLocation()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        map.uiSettings.isZoomControlsEnabled = true
+        map.uiSettings.isIndoorLevelPickerEnabled = true
+        map.uiSettings.isCompassEnabled = true
+        map.uiSettings.isMapToolbarEnabled = true
+    }
+
+    override fun getInfoContents(marker: Marker): View? = null
+
+    override fun getInfoWindow(marker: Marker): View? = null
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                getMyLocation()
+            } else {
+                Toast.makeText(requireContext(), "Can't access your location. Check your permissions for this app", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private fun getMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                requireActivity().applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            map.isMyLocationEnabled = true
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
 }
